9809c129c58364bf76ff5b5bf0161f40b579b63d
ljh0608 <m98m@catholic.ac.kr>
2023-06-10 19:18:53 +0900

    chore: 불필요한 파일 삭제

--- AdvancedWebProject/src/lib/hooks/useReqFollowing.ts
+++
@@ -1,7 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { followUser } from "lib/api";
-
-// console.log( { mutate, isLoading, isError, error, isSuccess }
-//   `isLoading: ${isLoading}, isError: ${isError}, error: ${error}, isSuccess: ${isSuccess}`
-// );
-// // useMutation의 리턴값을 출력한다.
